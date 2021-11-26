af3253f64c7ba40b919e25106a305898df104060
praveenaapoon <93528329+praveenaapoon@users.noreply.github.com>
2021-11-26 17:08:46 +0530

    Update urls.py

--- circles/urls.py
+++ circles/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('login.urls')),
+    path('capp/', include('Appcircle.urls')),
 ]
