aa364beffaaed0127f9e94d7c7ef14f35cb3080a
praveenaapoon <93528329+praveenaapoon@users.noreply.github.com>
2021-11-27 13:42:08 +0530

    Update models.py

--- Appcircle/models.py
+++ Appcircle/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from login.models import User
 
+class CircleUser(models.Model):
+    name = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, choices=[('admin', 'Admin'), ('mod', 'Moderator'), ('user', 'User')])
+    users = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Circle(models.Model):
+    circleuser = models.ManyToManyField(CircleUser)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
     image = models.ImageField()
@@ -10,12 +19,3 @@ class Circle(models.Model):
     
     def __str__(self):
         return self.name
-
-class CircleUser(models.Model):
-    circle = models.ManyToManyField(Circle)
-    name = models.CharField(max_length=50)
-    role = models.CharField(max_length=20, choices=[('admin', 'Admin'), ('mod', 'Moderator'), ('user', 'User')])
-    users = models.ForeignKey(to=User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.circle
